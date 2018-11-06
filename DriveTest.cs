d3173d80684a67c1766c60c86df33d9df664a2a7
dloy3000 <39393611+dloy3000@users.noreply.github.com>
2018-11-05 20:48:26 -0500

    Add files via upload
    test scripts

---
+++ DriveTest.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DriveTest : MonoBehaviour
+{
+    public Rigidbody2D car;
+
+    public float walkSpeed;
+
+    private Vector3 movementVector;
+
+    private float horizontalF, verticalF;
+
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalF = Input.GetAxisRaw("Horizontal") * walkSpeed;
+
+        verticalF = Input.GetAxisRaw("Vertical") * walkSpeed;
+
+        car.AddForce(new Vector2(horizontalF * walkSpeed, verticalF * walkSpeed));
+    }
+}
---
+++ moveTest.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moveTest : MonoBehaviour
+{
+    public Rigidbody2D player;
+
+    public float walkSpeed;
+
+    private float horizontalF, verticalF;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalF = Input.GetAxisRaw("Horizontal") * walkSpeed;
+
+        verticalF = Input.GetAxisRaw("Vertical") * walkSpeed;
+
+        player.velocity = new Vector2(horizontalF * walkSpeed, verticalF * walkSpeed);
+    }
+}

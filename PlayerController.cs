0f9b7dc7b121afc199fb7893f035fd26fa02f484
dloy3000 <39393611+dloy3000@users.noreply.github.com>
2018-11-27 14:38:31 -0500

    Add files via upload

---
+++ PlayerController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Rigidbody2D player;
+
+    public Animator anim;
+
+    public bool isWalking;
+
+    public float walkSpeed;
+
+    private float horizontalF, verticalF;
+
+    // Use this for initialization
+    void Start()
+    {
+        isWalking = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalF = Input.GetAxisRaw("Horizontal") * walkSpeed;
+
+        verticalF = Input.GetAxisRaw("Vertical") * walkSpeed;
+
+        player.velocity = new Vector2(horizontalF * walkSpeed, verticalF * walkSpeed);
+
+        Animate(verticalF, horizontalF);
+    }
+
+    void Animate(float verticalF, float horizontalF)
+    {
+        float currentVector = horizontalF + verticalF;
+
+        if (currentVector != 0)
+            isWalking = true;
+
+        else if (currentVector == 0)
+            isWalking = false;
+
+        anim.SetBool("isWalking", isWalking);
+    }
+
+    void Interact()
+    {
+
+    }
+}

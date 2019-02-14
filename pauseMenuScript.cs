10025e644a2967e90ad95edfaaed2d276f057e4d
dloy3000 <39393611+dloy3000@users.noreply.github.com>
2019-02-14 09:23:35 -0500

    Add files via upload

---
+++ pauseMenuScript.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class pauseMenuScript : MonoBehaviour
+{
+
+    public static bool isPaused = false;
+
+    public GameObject pauseMenuUI;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(isPaused)
+        {
+            resume();
+        }
+
+        else
+        {
+            pause();
+        }
+
+    }
+
+    void resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        isPaused = false;
+    }
+
+    void pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        isPaused = true;
+    }
+
+    public void loadMenu()
+    {
+        SceneManager.LoadScene("Menu");
+
+    }
+
+    public void quitGame()
+    {
+        Debug.Log("Application quit");
+        Application.Quit();
+    }
+
+
+
+
+}
